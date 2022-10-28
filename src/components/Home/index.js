a0594e19804ff66b0ebd1d929b53ad1272ba8138
Tyler Waugh <waughphoto@gmail.com>
2022-10-28 12:50:00 -0400

    Animated Letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,12 +7,13 @@ const Home = () => {
     return (
         <div className='container home-page'>
             <div className='text-zone'>
-                <h1>Hi, <br /> I'm
+               <h1> Hi, <br /> I'm
                 <img src={LogoTitle} alt='developer' />
                 lobodan
                 <br />
                 web developer
                 </h1>
+                
                 <h2>Frontend Developer / Javascript Expert</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
             </div>
